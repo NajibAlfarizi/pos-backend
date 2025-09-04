fad0751a40956219a88fbd5c3dc5fcd1000afbe3
Najib Alfarizi <najibalfarizi5@gmail.com>
2025-09-04 17:26:22 +0700

    refactor index.js to enhance API structure and add CORS support

--- api/index.js
+++ api/index.js
@@ -1,6 +1,47 @@
-import express from "express";
-const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+import cors from 'cors';
+import express from 'express';
+
+import authRoutes from '../src/routes/authRoutes.js';
+import merekRoutes from '../src/routes/merekRoutes.js';
+import kategoriBarangRoutes from '../src/routes/kategoriBarangRoutes.js';
+import sparepartRoutes from '../src/routes/sparepartRoutes.js';
+import transaksiRoutes from '../src/routes/transaksiRoutes.js';
+import laporanRoutes from '../src/routes/laporanRoutes.js';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API is working!' });
+});
+
+// CORS
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
+  })
+);
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/merek', merekRoutes);
+app.use('/kategori-barang', kategoriBarangRoutes);
+app.use('/sparepart', sparepartRoutes);
+app.use('/transaksi', transaksiRoutes);
+app.use('/laporan', laporanRoutes);
+
+// Jangan pakai app.listen()
 
 export default app;
